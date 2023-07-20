0de60c73d3db114aadb0b879a46d57dd90caa9a4
WenXin2 <dwnewville@gmail.com>
2023-07-20 01:40:24 -0500

    Add green warp pipe resources

--- src/main/java/com/wenxin2/warp_pipes/blocks/WarpPipeBlock.java
+++ src/main/java/com/wenxin2/warp_pipes/blocks/WarpPipeBlock.java
@@ -1,7 +1,9 @@
 package com.wenxin2.warp_pipes.blocks;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.Mirror;
@@ -9,19 +11,25 @@ import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
 public class WarpPipeBlock extends DirectionalBlock {
+    public static final BooleanProperty ENTRANCE = BooleanProperty.create("entrance");
 
     public WarpPipeBlock(BlockBehaviour.Properties properties) {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.UP));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.UP).setValue(ENTRANCE, Boolean.TRUE));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
+        stateBuilder.add(ENTRANCE, FACING);
     }
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext placeContext) {
         Direction direction = placeContext.getClickedFace();
-        BlockState blockstate = placeContext.getLevel().getBlockState(placeContext.getClickedPos().relative(direction.getOpposite()));
-        return blockstate.is(this) && blockstate.getValue(FACING) == direction ? this.defaultBlockState().setValue(FACING, direction.getOpposite()) : this.defaultBlockState().setValue(FACING, direction);
+        return this.defaultBlockState().setValue(FACING, direction);
     }
 
     @Override
@@ -35,7 +43,61 @@ public class WarpPipeBlock extends DirectionalBlock {
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
-        stateBuilder.add(FACING);
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor worldAccessor, BlockPos pos, BlockPos pos2) {
+        Block blockAbove = worldAccessor.getBlockState(pos.above()).getBlock();
+        Block blockBelow = worldAccessor.getBlockState(pos.below()).getBlock();
+        Block blockNorth = worldAccessor.getBlockState(pos.north()).getBlock();
+        Block blockSouth = worldAccessor.getBlockState(pos.south()).getBlock();
+        Block blockEast = worldAccessor.getBlockState(pos.east()).getBlock();
+        Block blockWest = worldAccessor.getBlockState(pos.west()).getBlock();
+
+        boolean facingUp = state.getValue(FACING) == Direction.UP;
+        boolean facingDown = state.getValue(FACING) == Direction.DOWN;
+        boolean facingNorth = state.getValue(FACING) == Direction.NORTH;
+        boolean facingSouth = state.getValue(FACING) == Direction.SOUTH;
+        boolean facingEast = state.getValue(FACING) == Direction.EAST;
+        boolean facingWest = state.getValue(FACING) == Direction.WEST;
+
+        if (facingUp) {
+            if (blockAbove instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE);
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE);
+        }
+
+        if (facingDown) {
+            if (blockBelow instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE);
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE);
+        }
+
+        if (facingNorth) {
+            if (blockNorth instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE);
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE);
+        }
+
+        if (facingSouth) {
+            if (blockSouth instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE);
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE);
+        }
+
+        if (facingEast) {
+            if (blockEast instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE);
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE);
+        }
+
+        if (facingWest) {
+            if (blockWest instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE);
+            }
+        }
+        return state.setValue(ENTRANCE, Boolean.TRUE);
     }
 }
