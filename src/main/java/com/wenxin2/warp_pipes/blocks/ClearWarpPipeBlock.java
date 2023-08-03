161ffbbe0099298604f2b6db3706111cd3d8bcb9
WenXin2 <dwnewville@gmail.com>
2023-08-03 00:59:46 -0500

    Add directional blockstates
    - Add north/south models

--- src/main/java/com/wenxin2/warp_pipes/blocks/ClearWarpPipeBlock.java
+++ src/main/java/com/wenxin2/warp_pipes/blocks/ClearWarpPipeBlock.java
@@ -1,63 +1,170 @@
 package com.wenxin2.warp_pipes.blocks;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.wenxin2.warp_pipes.blocks.entities.WarpPipeBlockEntity;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
+import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.network.protocol.game.ClientboundLevelParticlesPacket;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.gameevent.GameEvent;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.LavaFluid;
-import net.minecraft.world.level.material.WaterFluid;
-import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraftforge.server.ServerLifecycleHooks;
 
 public class ClearWarpPipeBlock extends WarpPipeBlock implements EntityBlock {
     public static final BooleanProperty ENTRANCE = BooleanProperty.create("entrance");
     public static final BooleanProperty CLOSED = BooleanProperty.create("closed");
+    //    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    public static final BooleanProperty UP = BlockStateProperties.UP;
+    public static final BooleanProperty DOWN = BlockStateProperties.DOWN;
+    public static final BooleanProperty NORTH = BlockStateProperties.NORTH;
+    public static final BooleanProperty EAST = BlockStateProperties.EAST;
+    public static final BooleanProperty SOUTH = BlockStateProperties.SOUTH;
+    public static final BooleanProperty WEST = BlockStateProperties.WEST;
+    public static final Map<Direction, BooleanProperty> PROPERTY_BY_DIRECTION = ImmutableMap.copyOf(Util.make(Maps.newEnumMap(Direction.class), (enumMap) -> {
+        enumMap.put(Direction.UP, UP);
+        enumMap.put(Direction.DOWN, DOWN);
+        enumMap.put(Direction.NORTH, NORTH);
+        enumMap.put(Direction.EAST, EAST);
+        enumMap.put(Direction.SOUTH, SOUTH);
+        enumMap.put(Direction.WEST, WEST);
+    }));
+
+    public static final VoxelShape PIPE_ENTRANCE = Shapes.or(
+            Block.box(0, 0, 0, 16, 16, 3),
+            Block.box(0, 0, 13, 16, 16, 16),
+            Block.box(13, 0, 0, 16, 16, 16),
+            Block.box(0, 0, 0, 3, 16, 16),
+            Block.box(0, 0, 0, 16, 3, 16)).optimize();
+
+    public static final VoxelShape PIPE_ENTRANCE_N = Shapes.or(
+            Block.box(0, 0, 13, 16, 16, 16),
+            Block.box(13, 0, 0, 16, 16, 16),
+            Block.box(0, 0, 0, 3, 16, 16),
+            Block.box(0, 0, 0, 16, 3, 16)).optimize();
+
+    public static final VoxelShape PIPE_ENTRANCE_S = Shapes.or(
+            Block.box(0, 0, 0, 16, 16, 3),
+            Block.box(13, 0, 0, 16, 16, 16),
+            Block.box(0, 0, 0, 3, 16, 16),
+            Block.box(0, 0, 0, 16, 3, 16)).optimize();
+
+    public static final VoxelShape PIPE_ENTRANCE_E = Shapes.or(
+            Block.box(0, 0, 0, 16, 16, 3),
+            Block.box(13, 0, 0, 16, 16, 16),
+            Block.box(0, 0, 0, 16, 3, 16),
+            Block.box(0, 0, 0, 3, 16, 16)
+    ).optimize();
+
+    public static final VoxelShape PIPE_ENTRANCE_W = Shapes.or(
+            Block.box(0, 0, 0, 3, 16, 16),
+            Block.box(0, 0, 0, 16, 16, 3),
+            Block.box(0, 0, 13, 16, 16, 16),
+            Block.box(0, 0, 0, 16, 3, 16)
+    ).optimize();
 
     public ClearWarpPipeBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.UP).setValue(ENTRANCE, Boolean.TRUE).setValue(CLOSED, Boolean.FALSE));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.UP).setValue(ENTRANCE, Boolean.TRUE).setValue(CLOSED, Boolean.FALSE)
+                .setValue(UP, Boolean.FALSE).setValue(NORTH, Boolean.FALSE).setValue(SOUTH, Boolean.FALSE)
+                .setValue(EAST, Boolean.FALSE).setValue(WEST, Boolean.FALSE).setValue(DOWN, Boolean.FALSE));
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
-        stateBuilder.add(CLOSED, ENTRANCE, FACING);
+        stateBuilder.add(CLOSED, ENTRANCE, FACING, UP, DOWN, NORTH, SOUTH, EAST, WEST);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext context) {
+        if (state.getValue(ENTRANCE)) {
+            if (state.getValue(NORTH)) {
+                if (state.getValue(CLOSED)) {
+                    return PIPE_ENTRANCE_N;
+                } else {
+                    return PIPE_ENTRANCE_N;
+                }
+            }
+            if (state.getValue(SOUTH)) {
+                if (state.getValue(CLOSED)) {
+                    return PIPE_ENTRANCE_S;
+                } else {
+                    return PIPE_ENTRANCE_S;
+                }
+            }
+            if (state.getValue(EAST)) {
+                if (state.getValue(CLOSED)) {
+                    return PIPE_ENTRANCE_E;
+                } else {
+                    return PIPE_ENTRANCE_E;
+                }
+            }
+            if (state.getValue(WEST)) {
+                if (state.getValue(CLOSED)) {
+                    return PIPE_ENTRANCE_W;
+                } else {
+                    return PIPE_ENTRANCE_W;
+                }
+            }
+            return PIPE_ENTRANCE;
+        }
+        else return PIPE_ENTRANCE;
     }
 
     @Override
     public BlockEntity newBlockEntity(final BlockPos pos, final BlockState state)
     {
         return new WarpPipeBlockEntity(pos, state);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext placeContext) {
+        FluidState fluidState = placeContext.getLevel().getFluidState(placeContext.getClickedPos());
+        Direction direction = placeContext.getClickedFace();
+        BlockGetter blockGetter = placeContext.getLevel();
+        BlockPos pos = placeContext.getClickedPos();
+
+        BlockPos posAbove = pos.above();
+        BlockPos posBelow = pos.below();
+        BlockPos posNorth = pos.north();
+        BlockPos posSouth = pos.south();
+        BlockPos posEast = pos.east();
+        BlockPos posWest = pos.west();
+
+        BlockState stateAbove = blockGetter.getBlockState(posAbove);
+        BlockState stateBelow = blockGetter.getBlockState(posBelow);
+        BlockState stateNorth = blockGetter.getBlockState(posNorth);
+        BlockState stateSouth = blockGetter.getBlockState(posSouth);
+        BlockState stateEast = blockGetter.getBlockState(posEast);
+        BlockState stateWest = blockGetter.getBlockState(posWest);
+
+        return this.defaultBlockState().setValue(FACING, direction)
+                .setValue(UP, this.connectsTo(stateAbove))
+                .setValue(DOWN, this.connectsTo(stateBelow))
+                .setValue(NORTH, this.connectsTo(stateNorth))
+                .setValue(SOUTH, this.connectsTo(stateSouth))
+                .setValue(EAST, this.connectsTo(stateEast))
+                .setValue(WEST, this.connectsTo(stateWest))
+                .setValue(CLOSED, placeContext.getLevel().hasNeighborSignal(placeContext.getClickedPos()));
+    }
+
+    public boolean connectsTo(BlockState state) {
+        Block block = state.getBlock();
+        return block instanceof ClearWarpPipeBlock;
     }
 
     @Override
@@ -73,5 +180,68 @@ public class ClearWarpPipeBlock extends WarpPipeBlock implements EntityBlock {
     @Override
     public boolean skipRendering(BlockState state, BlockState neighborState, Direction direction) {
         return neighborState.is(this) ? true : super.skipRendering(state, neighborState, direction);
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor worldAccessor, BlockPos pos, BlockPos pos2) {
+        Block blockAbove = worldAccessor.getBlockState(pos.above()).getBlock();
+        Block blockBelow = worldAccessor.getBlockState(pos.below()).getBlock();
+        Block blockNorth = worldAccessor.getBlockState(pos.north()).getBlock();
+        Block blockSouth = worldAccessor.getBlockState(pos.south()).getBlock();
+        Block blockEast = worldAccessor.getBlockState(pos.east()).getBlock();
+        Block blockWest = worldAccessor.getBlockState(pos.west()).getBlock();
+
+        boolean facingUp = state.getValue(FACING) == Direction.UP;
+        boolean facingDown = state.getValue(FACING) == Direction.DOWN;
+        boolean facingNorth = state.getValue(FACING) == Direction.NORTH;
+        boolean facingSouth = state.getValue(FACING) == Direction.SOUTH;
+        boolean facingEast = state.getValue(FACING) == Direction.EAST;
+        boolean facingWest = state.getValue(FACING) == Direction.WEST;
+
+        if (facingUp && direction == Direction.UP && neighborState.is(Blocks.WATER) && !state.getValue(CLOSED)) {
+            worldAccessor.scheduleTick(pos, this, 20);
+        }
+
+        if (facingUp) {
+            if (blockAbove instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+        }
+
+        if (facingDown) {
+            if (blockBelow instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+        }
+
+        if (facingNorth) {
+            if (blockNorth instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+        }
+
+        if (facingSouth) {
+            if (blockSouth instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+        }
+
+        if (facingEast) {
+            if (blockEast instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+            }
+            return state.setValue(ENTRANCE, Boolean.TRUE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+        }
+
+        if (facingWest) {
+            if (blockWest instanceof WarpPipeBlock) {
+                return state.setValue(ENTRANCE, Boolean.FALSE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
+            }
+        }
+        return state.setValue(ENTRANCE, Boolean.TRUE).setValue(PROPERTY_BY_DIRECTION.get(direction), this.connectsTo(neighborState));
     }
 }
