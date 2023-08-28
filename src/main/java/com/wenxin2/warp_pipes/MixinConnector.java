fcfc200de83888eea5e5713534101cc3dd0d430d
WenXin2 <dwnewville@gmail.com>
2023-08-27 23:02:11 -0500

    Fix mixin crash

---
+++ src/main/java/com/wenxin2/warp_pipes/MixinConnector.java
@@ -0,0 +1,11 @@
+package com.wenxin2.warp_pipes;
+
+import org.spongepowered.asm.mixin.Mixins;
+import org.spongepowered.asm.mixin.connect.IMixinConnector;
+
+public class MixinConnector implements IMixinConnector {
+    @Override
+    public void connect() {
+        Mixins.addConfigurations("warp_pipes.mixins.json");
+    }
+}
