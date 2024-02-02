55e72362484f6345c0d1abbbfd4acc6e2e2a8267
WenXin2 <dwnewville@gmail.com>
2024-02-02 00:33:01 -0600

    Button textures

--- src/main/java/com/wenxin2/warp_pipes/blocks/WaterSpoutBlock.java
+++ src/main/java/com/wenxin2/warp_pipes/blocks/WaterSpoutBlock.java
@@ -1,6 +1,5 @@
 package com.wenxin2.warp_pipes.blocks;
 
-import com.wenxin2.warp_pipes.blocks.client.WarpPipeScreen;
 import com.wenxin2.warp_pipes.blocks.entities.WarpPipeBlockEntity;
 import com.wenxin2.warp_pipes.init.Config;
 import com.wenxin2.warp_pipes.init.ModRegistry;
@@ -26,11 +25,9 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.AirBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BucketPickup;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
