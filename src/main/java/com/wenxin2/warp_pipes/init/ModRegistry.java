aa00a246fd6696f3f757b4379d7054fd5af8d4cf
WenXin2 <dwnewville@gmail.com>
2024-05-08 21:59:13 -0500

    Test changes to search pipe uuid

--- src/main/java/com/wenxin2/warp_pipes/init/ModRegistry.java
+++ src/main/java/com/wenxin2/warp_pipes/init/ModRegistry.java
@@ -90,7 +90,7 @@ public class ModRegistry {
 
         GREEN_WARP_PIPE = registerBlock("green_warp_pipe",
                 () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
+                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always).randomTicks()
                         .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
 
         LIGHT_BLUE_WARP_PIPE = registerBlock("light_blue_warp_pipe",
