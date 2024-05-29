4162da16dd77dfe2cee5997d332e692dff3d8f12
WenXin2 <dwnewville@gmail.com>
2024-05-28 20:56:16 -0500

    Change to dynamic registry of warp pipes

--- src/main/java/com/wenxin2/warp_pipes/init/ModCreativeTabs.java
+++ src/main/java/com/wenxin2/warp_pipes/init/ModCreativeTabs.java
@@ -5,9 +5,11 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,7 +21,7 @@ public class ModCreativeTabs {
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, WarpPipes.MODID);
     public static final RegistryObject<CreativeModeTab> WARP_PIPES_TAB = TABS.register("warp_pipes_tab", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.warp_pipes"))
-            .icon(() -> new ItemStack(ModRegistry.GREEN_WARP_PIPE.get())).build());
+            .icon(() -> new ItemStack(ModRegistry.WARP_PIPES.get(DyeColor.GREEN).get())).build());
 
     @SubscribeEvent
     public static void addCreative(BuildCreativeModeTabContentsEvent event) {
@@ -27,22 +29,10 @@ public class ModCreativeTabs {
             add(event, ModRegistry.PIPE_WRENCH.get());
 
             add(event, ModRegistry.CLEAR_WARP_PIPE.get());
-            add(event, ModRegistry.WHITE_WARP_PIPE.get());
-            add(event, ModRegistry.LIGHT_GRAY_WARP_PIPE.get());
-            add(event, ModRegistry.GRAY_WARP_PIPE.get());
-            add(event, ModRegistry.BLACK_WARP_PIPE.get());
-            add(event, ModRegistry.BROWN_WARP_PIPE.get());
-            add(event, ModRegistry.RED_WARP_PIPE.get());
-            add(event, ModRegistry.ORANGE_WARP_PIPE.get());
-            add(event, ModRegistry.YELLOW_WARP_PIPE.get());
-            add(event, ModRegistry.LIME_WARP_PIPE.get());
-            add(event, ModRegistry.GREEN_WARP_PIPE.get());
-            add(event, ModRegistry.CYAN_WARP_PIPE.get());
-            add(event, ModRegistry.LIGHT_BLUE_WARP_PIPE.get());
-            add(event, ModRegistry.BLUE_WARP_PIPE.get());
-            add(event, ModRegistry.PURPLE_WARP_PIPE.get());
-            add(event, ModRegistry.MAGENTA_WARP_PIPE.get());
-            add(event, ModRegistry.PINK_WARP_PIPE.get());
+
+            for (RegistryObject<Block> pipe : ModRegistry.WARP_PIPES.values()) {
+                add(event, pipe.get());
+            }
         }
         if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             addAfter(event, Items.FISHING_ROD, ModRegistry.PIPE_WRENCH.get());
@@ -54,47 +44,23 @@ public class ModCreativeTabs {
 
         if (event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS) {
             addAfter(event, Items.REDSTONE_LAMP, ModRegistry.CLEAR_WARP_PIPE.get());
-            addAfter(event, Items.REDSTONE_LAMP, ModRegistry.GREEN_WARP_PIPE.get());
+            addAfter(event, Items.REDSTONE_LAMP, ModRegistry.WARP_PIPES.get(DyeColor.GREEN).get());
         }
 
         if (event.getTabKey() == CreativeModeTabs.COLORED_BLOCKS) {
             add(event, ModRegistry.CLEAR_WARP_PIPE.get());
-            add(event, ModRegistry.WHITE_WARP_PIPE.get());
-            add(event, ModRegistry.LIGHT_GRAY_WARP_PIPE.get());
-            add(event, ModRegistry.GRAY_WARP_PIPE.get());
-            add(event, ModRegistry.BLACK_WARP_PIPE.get());
-            add(event, ModRegistry.BROWN_WARP_PIPE.get());
-            add(event, ModRegistry.RED_WARP_PIPE.get());
-            add(event, ModRegistry.ORANGE_WARP_PIPE.get());
-            add(event, ModRegistry.YELLOW_WARP_PIPE.get());
-            add(event, ModRegistry.LIME_WARP_PIPE.get());
-            add(event, ModRegistry.GREEN_WARP_PIPE.get());
-            add(event, ModRegistry.CYAN_WARP_PIPE.get());
-            add(event, ModRegistry.LIGHT_BLUE_WARP_PIPE.get());
-            add(event, ModRegistry.BLUE_WARP_PIPE.get());
-            add(event, ModRegistry.PURPLE_WARP_PIPE.get());
-            add(event, ModRegistry.MAGENTA_WARP_PIPE.get());
-            add(event, ModRegistry.PINK_WARP_PIPE.get());
+
+            for (RegistryObject<Block> pipe : ModRegistry.WARP_PIPES.values()) {
+                add(event, pipe.get());
+            }
         }
 
         if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
             addAfter(event, Items.RESPAWN_ANCHOR, ModRegistry.CLEAR_WARP_PIPE.get());
-            addAfter(event, ModRegistry.CLEAR_WARP_PIPE.get(), ModRegistry.WHITE_WARP_PIPE.get());
-            addAfter(event, ModRegistry.WHITE_WARP_PIPE.get(), ModRegistry.LIGHT_GRAY_WARP_PIPE.get());
-            addAfter(event, ModRegistry.LIGHT_GRAY_WARP_PIPE.get(), ModRegistry.GRAY_WARP_PIPE.get());
-            addAfter(event, ModRegistry.GRAY_WARP_PIPE.get(), ModRegistry.BLACK_WARP_PIPE.get());
-            addAfter(event, ModRegistry.BLACK_WARP_PIPE.get(), ModRegistry.BROWN_WARP_PIPE.get());
-            addAfter(event, ModRegistry.BROWN_WARP_PIPE.get(), ModRegistry.RED_WARP_PIPE.get());
-            addAfter(event, ModRegistry.RED_WARP_PIPE.get(), ModRegistry.ORANGE_WARP_PIPE.get());
-            addAfter(event, ModRegistry.ORANGE_WARP_PIPE.get(), ModRegistry.YELLOW_WARP_PIPE.get());
-            addAfter(event, ModRegistry.YELLOW_WARP_PIPE.get(), ModRegistry.LIME_WARP_PIPE.get());
-            addAfter(event, ModRegistry.LIME_WARP_PIPE.get(), ModRegistry.GREEN_WARP_PIPE.get());
-            addAfter(event, ModRegistry.GREEN_WARP_PIPE.get(), ModRegistry.CYAN_WARP_PIPE.get());
-            addAfter(event, ModRegistry.CYAN_WARP_PIPE.get(), ModRegistry.LIGHT_BLUE_WARP_PIPE.get());
-            addAfter(event, ModRegistry.LIGHT_BLUE_WARP_PIPE.get(), ModRegistry.BLUE_WARP_PIPE.get());
-            addAfter(event, ModRegistry.BLUE_WARP_PIPE.get(), ModRegistry.PURPLE_WARP_PIPE.get());
-            addAfter(event, ModRegistry.PURPLE_WARP_PIPE.get(), ModRegistry.MAGENTA_WARP_PIPE.get());
-            addAfter(event, ModRegistry.MAGENTA_WARP_PIPE.get(), ModRegistry.PINK_WARP_PIPE.get());
+
+            for (RegistryObject<Block> pipe : ModRegistry.WARP_PIPES.values()) {
+                addAfter(event, ModRegistry.CLEAR_WARP_PIPE.get(), pipe.get());
+            }
         }
     }
 
--- src/main/java/com/wenxin2/warp_pipes/init/ModRegistry.java
+++ src/main/java/com/wenxin2/warp_pipes/init/ModRegistry.java
@@ -1,6 +1,5 @@
 package com.wenxin2.warp_pipes.init;
 
-import com.mojang.datafixers.types.Type;
 import com.wenxin2.warp_pipes.WarpPipes;
 import com.wenxin2.warp_pipes.blocks.ClearWarpPipeBlock;
 import com.wenxin2.warp_pipes.blocks.PipeBubblesBlock;
@@ -9,11 +8,15 @@ import com.wenxin2.warp_pipes.blocks.WaterSpoutBlock;
 import com.wenxin2.warp_pipes.blocks.entities.WarpPipeBlockEntity;
 import com.wenxin2.warp_pipes.inventory.WarpPipeMenu;
 import com.wenxin2.warp_pipes.items.WrenchItem;
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.level.BlockGetter;
@@ -29,33 +32,24 @@ import net.minecraftforge.registries.RegistryObject;
 
 @Mod.EventBusSubscriber(modid = WarpPipes.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModRegistry {
+    public static final EnumMap<DyeColor, RegistryObject<Block>> WARP_PIPES =
+            new EnumMap<>(DyeColor.class);
     public static final RegistryObject<Item> PIPE_WRENCH;
-
-    public static final RegistryObject<Block> BLACK_WARP_PIPE;
-    public static final RegistryObject<Block> BLUE_WARP_PIPE;
-    public static final RegistryObject<Block> BROWN_WARP_PIPE;
     public static final RegistryObject<Block> CLEAR_WARP_PIPE;
-    public static final RegistryObject<Block> CYAN_WARP_PIPE;
-    public static final RegistryObject<Block> GRAY_WARP_PIPE;
-    public static final RegistryObject<Block> GREEN_WARP_PIPE;
-    public static final RegistryObject<Block> LIGHT_BLUE_WARP_PIPE;
-    public static final RegistryObject<Block> LIGHT_GRAY_WARP_PIPE;
-    public static final RegistryObject<Block> LIME_WARP_PIPE;
-    public static final RegistryObject<Block> MAGENTA_WARP_PIPE;
-    public static final RegistryObject<Block> ORANGE_WARP_PIPE;
-    public static final RegistryObject<Block> PINK_WARP_PIPE;
     public static final RegistryObject<Block> PIPE_BUBBLES;
-    public static final RegistryObject<Block> PURPLE_WARP_PIPE;
-    public static final RegistryObject<Block> RED_WARP_PIPE;
     public static final RegistryObject<Block> WATER_SPOUT;
-    public static final RegistryObject<Block> WHITE_WARP_PIPE;
-    public static final RegistryObject<Block> YELLOW_WARP_PIPE;
 
     public static final RegistryObject<BlockEntityType<WarpPipeBlockEntity>> WARP_PIPE_BLOCK_ENTITY;
     public static final RegistryObject<MenuType<WarpPipeMenu>> WARP_PIPE_MENU;
 
     static
     {
+        Arrays.stream(DyeColor.values()).forEach(color ->
+                WARP_PIPES.put(color, registerBlock(color.getName() + "_warp_pipe",
+                        () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(color)
+                                .sound(SoundType.NETHERITE_BLOCK).strength(3.5F, 1000.0F)
+                                .isViewBlocking(ModRegistry::always).requiresCorrectToolForDrops()))));
+
         PIPE_WRENCH = registerItem("pipe_wrench",
                 () -> new WrenchItem(new Item.Properties().durability(128), Tiers.IRON));
 
@@ -63,88 +57,6 @@ public class ModRegistry {
                 () -> new ClearWarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.NONE)
                         .sound(SoundType.GLASS).isSuffocating(ModRegistry::never).isViewBlocking(ModRegistry::never)
                         .strength(3.0F, 500.0F).requiresCorrectToolForDrops().noOcclusion()));
-
-        BLACK_WARP_PIPE = registerBlock("black_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK)
-                        .sound(SoundType.NETHERITE_BLOCK).strength(3.5F, 1000.0F)
-                        .isViewBlocking(ModRegistry::always).requiresCorrectToolForDrops()));
-
-        BLUE_WARP_PIPE = registerBlock("blue_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLUE)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        BROWN_WARP_PIPE = registerBlock("brown_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BROWN)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        CYAN_WARP_PIPE = registerBlock("cyan_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_CYAN)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        GRAY_WARP_PIPE = registerBlock("gray_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GRAY)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        GREEN_WARP_PIPE = registerBlock("green_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        LIGHT_BLUE_WARP_PIPE = registerBlock("light_blue_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_LIGHT_BLUE)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        LIGHT_GRAY_WARP_PIPE = registerBlock("light_gray_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_LIGHT_GRAY)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        LIME_WARP_PIPE = registerBlock("lime_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_LIGHT_GREEN)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        MAGENTA_WARP_PIPE = registerBlock("magenta_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_MAGENTA)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        ORANGE_WARP_PIPE = registerBlock("orange_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_ORANGE)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        PINK_WARP_PIPE = registerBlock("pink_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_PINK)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        PURPLE_WARP_PIPE = registerBlock("purple_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_PURPLE)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        RED_WARP_PIPE = registerBlock("red_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_RED)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F)
-                        .requiresCorrectToolForDrops()));
-
-        WHITE_WARP_PIPE = registerBlock("white_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.SNOW)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
-        YELLOW_WARP_PIPE = registerBlock("yellow_warp_pipe",
-                () -> new WarpPipeBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW)
-                        .sound(SoundType.NETHERITE_BLOCK).isViewBlocking(ModRegistry::always)
-                        .strength(3.5F, 1000.0F).requiresCorrectToolForDrops()));
-
 
         PIPE_BUBBLES = registerNoItemBlock("pipe_bubbles",
                 () -> new PipeBubblesBlock(BlockBehaviour.Properties.of().pushReaction(PushReaction.DESTROY)
@@ -156,15 +68,10 @@ public class ModRegistry {
                         .isSuffocating(ModRegistry::never).isViewBlocking(ModRegistry::never)
                         .replaceable().noCollission().noLootTable()));
 
-
         WARP_PIPE_BLOCK_ENTITY = WarpPipes.BLOCK_ENTITIES.register("warp_pipe",
                 () -> BlockEntityType.Builder.of(WarpPipeBlockEntity::new,
-                        ModRegistry.CLEAR_WARP_PIPE.get(), ModRegistry.BLACK_WARP_PIPE.get(), ModRegistry.BLUE_WARP_PIPE.get(),
-                                ModRegistry.BROWN_WARP_PIPE.get(), ModRegistry.CYAN_WARP_PIPE.get(), ModRegistry.GRAY_WARP_PIPE.get(),
-                                ModRegistry.GREEN_WARP_PIPE.get(), ModRegistry.LIGHT_BLUE_WARP_PIPE.get(), ModRegistry.LIGHT_GRAY_WARP_PIPE.get(),
-                                ModRegistry.LIME_WARP_PIPE.get(), ModRegistry.MAGENTA_WARP_PIPE.get(), ModRegistry.ORANGE_WARP_PIPE.get(),
-                                ModRegistry.PINK_WARP_PIPE.get(), ModRegistry.PURPLE_WARP_PIPE.get(), ModRegistry.RED_WARP_PIPE.get(),
-                                ModRegistry.WHITE_WARP_PIPE.get(), ModRegistry.YELLOW_WARP_PIPE.get())
+                                Stream.concat(WARP_PIPES.values().stream().map(RegistryObject::get),
+                                        Stream.of(CLEAR_WARP_PIPE.get())).toArray(Block[]::new))
                         .build(null));
 
         WARP_PIPE_MENU = WarpPipes.MENUS.register("warp_pipe", () -> new MenuType<>(WarpPipeMenu::new, FeatureFlags.REGISTRY.allFlags()));
@@ -192,6 +99,7 @@ public class ModRegistry {
     {
         return true;
     }
+
     private static boolean never(BlockState state, BlockGetter block, BlockPos pos)
     {
         return false;
