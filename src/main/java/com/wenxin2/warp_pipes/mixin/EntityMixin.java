8cb4ec124f4c21c10cf2d77bdd0ff31b376490fe
WenXin2 <dwnewville@gmail.com>
2024-08-29 20:29:44 -0500

    Remove unused shadows

--- src/main/java/com/wenxin2/warp_pipes/mixin/EntityMixin.java
+++ src/main/java/com/wenxin2/warp_pipes/mixin/EntityMixin.java
@@ -5,7 +5,6 @@ import com.wenxin2.warp_pipes.blocks.entities.WarpPipeBlockEntity;
 import com.wenxin2.warp_pipes.init.Config;
 import com.wenxin2.warp_pipes.init.ModTags;
 import java.util.Collection;
-import javax.annotation.Nullable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
@@ -14,13 +13,11 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -44,9 +41,6 @@ public abstract class EntityMixin {
     @Shadow public abstract int getId();
 
     @Shadow public abstract BlockPos blockPosition();
-    @Shadow @Final protected RandomSource random;
-
-    @Shadow public abstract int getBlockY();
 
     @Shadow public abstract double getRandomX(double p_20209_);
 
@@ -56,21 +50,7 @@ public abstract class EntityMixin {
 
     @Shadow public abstract EntityType<?> getType();
 
-    @Shadow public abstract void setDeltaMovement(Vec3 p_20257_);
-
-    @Shadow public abstract void resetFallDistance();
-
-    @Shadow public abstract Vec3 getDeltaMovement();
-
-    @Shadow @Nullable public abstract Entity getFirstPassenger();
-
     @Shadow public abstract void setPos(Vec3 p_146885_);
-
-    @Shadow public abstract boolean isVehicle();
-
-    @Shadow public abstract void move(MoverType p_19973_, Vec3 p_19974_);
-
-    @Shadow public abstract boolean isInWater();
 
     @Unique
     private static final int MAX_PARTICLE_AMOUNT = 100;
