5d984ab4466152478917519ac5772dcf0a51dcb5
WenXin2 <dwnewville@gmail.com>
2025-11-02 21:25:36 -0600

    Add sounds

--- src/main/java/com/wenxin2/warp_pipes/registries/SoundRegistry.java
+++ src/main/java/com/wenxin2/warp_pipes/registries/SoundRegistry.java
@@ -8,8 +8,13 @@ import net.minecraft.world.level.block.SoundType;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class SoundRegistry {
-    public static final SoundType WATER_SPOUT = new SoundType(1.0F, 1.0F, SoundEvents.BUCKET_FILL,
-            SoundEvents.BUCKET_FILL, SoundEvents.BUCKET_EMPTY, SoundEvents.BUCKET_FILL, SoundEvents.BUCKET_FILL);
+    public static final DeferredHolder<SoundEvent, SoundEvent> CLEAR_PIPE_ENTER;
+    public static final DeferredHolder<SoundEvent, SoundEvent> CLEAR_PIPE_EXIT;
+    public static final DeferredHolder<SoundEvent, SoundEvent> CLEAR_PIPE_INSIDE;
+    public static final DeferredHolder<SoundEvent, SoundEvent> GLASS_HIT;
+    public static final DeferredHolder<SoundEvent, SoundEvent> GLASS_FALL;
+    public static final DeferredHolder<SoundEvent, SoundEvent> GLASS_STEP;
+    public static final DeferredHolder<SoundEvent, SoundEvent> ITEM_SPAWNS;
     public static final DeferredHolder<SoundEvent, SoundEvent> PIPES_LINKED;
     public static final DeferredHolder<SoundEvent, SoundEvent> PIPE_CLOSES;
     public static final DeferredHolder<SoundEvent, SoundEvent> PIPE_OPENS;
@@ -22,6 +27,23 @@ public class SoundRegistry {
     public static final DeferredHolder<SoundEvent, SoundEvent> WRENCH_BOUND;
 
     static {
+        CLEAR_PIPE_ENTER = WarpPipes.SOUNDS.register("block.clear_pipe_enter",
+                () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.clear_pipe_enter")));
+        CLEAR_PIPE_EXIT = WarpPipes.SOUNDS.register("block.clear_pipe_exit",
+                () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.clear_pipe_exit")));
+        CLEAR_PIPE_INSIDE = WarpPipes.SOUNDS.register("block.clear_pipe_inside",
+                () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.clear_pipe_inside")));
+
+        GLASS_HIT = WarpPipes.SOUNDS.register("block.glass_hit",
+                () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.glass_hit")));
+        GLASS_FALL = WarpPipes.SOUNDS.register("block.glass_fall",
+                () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.glass_fall")));
+        GLASS_STEP = WarpPipes.SOUNDS.register("block.glass_step",
+                () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.glass_step")));
+
+        ITEM_SPAWNS = WarpPipes.SOUNDS.register("block.item_spawns",
+                () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.item_spawns")));
+        
         PIPES_LINKED = WarpPipes.SOUNDS.register("block.pipes_linked",
                 () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.pipes_linked")));
         PIPE_CLOSES = WarpPipes.SOUNDS.register("block.pipe_closes",
@@ -30,6 +52,7 @@ public class SoundRegistry {
                 () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.pipe_opens")));
         PIPE_WARPS = WarpPipes.SOUNDS.register("block.pipe_warps",
                 () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.pipe_warps")));
+        
         WATER_SPOUT_BREAK = WarpPipes.SOUNDS.register("block.water_spout.break",
                 () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.water_spout.break")));
         WATER_SPOUT_FALL = WarpPipes.SOUNDS.register("block.water_spout.fall",
@@ -40,6 +63,7 @@ public class SoundRegistry {
                 () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.water_spout.place")));
         WATER_SPOUT_STEP = WarpPipes.SOUNDS.register("block.water_spout.step",
                 () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "block.water_spout.step")));
+        
         WRENCH_BOUND = WarpPipes.SOUNDS.register("item.wrench_bound",
                 () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(WarpPipes.MOD_ID, "item.wrench_bound")));
     }
