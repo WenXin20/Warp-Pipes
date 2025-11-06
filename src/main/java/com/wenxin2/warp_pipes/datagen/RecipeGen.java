829f9db5429ebb3fa3cce0983daf44c621c5222d
WenXin2 <dwnewville@gmail.com>
2025-11-05 19:25:09 -0600

    Add recipe to clear data

--- src/main/java/com/wenxin2/warp_pipes/datagen/RecipeGen.java
+++ src/main/java/com/wenxin2/warp_pipes/datagen/RecipeGen.java
@@ -1,5 +1,6 @@
 package com.wenxin2.warp_pipes.datagen;
 
+import com.wenxin2.warp_pipes.WarpPipes;
 import com.wenxin2.warp_pipes.registries.ModRegistry;
 import com.wenxin2.warp_pipes.registries.TagRegistry;
 import java.util.Map;
@@ -23,6 +24,7 @@ public class RecipeGen extends RecipeUtils {
 
     @Override
     protected void buildRecipes(RecipeOutput output) {
+        oneToOneConversionRecipe(output, ModRegistry.WRENCH, ModRegistry.WRENCH, WarpPipes.MOD_ID + ":" + getSimpleRecipeName(ModRegistry.WRENCH));
         twoItemTagRecipe(1, "warp_pipes", "_from_glass", ModRegistry.CLEAR_WARP_PIPE, RecipeCategory.BUILDING_BLOCKS, TagRegistry.DYEABLE_WARP_PIPE_ITEMS, Tags.Items.GLASS_BLOCKS_COLORLESS, output);
         warpDisruptorRecipe(1, ModRegistry.WARP_DISRUPTOR, Items.ENDER_EYE, Tags.Items.INGOTS_GOLD, Tags.Items.RODS_WOODEN, output);
         warpPipeRecipe(4, ModRegistry.CLEAR_WARP_PIPE, Tags.Items.INGOTS_COPPER, Tags.Items.GLASS_BLOCKS_COLORLESS, Tags.Items.GEMS_DIAMOND, Tags.Items.ENDER_PEARLS, output);
